2e8d6412930e8320cdda6698c2e7d194086da3c0
rina-reimer <rinareimer616@gmail.com>
2024-07-11 10:12:57 -0700

    completed projects page

---
+++ src/app/components/ProjectCard.js
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import {Divider} from "@nextui-org/divider";
+import { useState } from "react";
+import data from '/src/lib/en.json';
+import { SparklesIcon, BookOpenIcon } from "@heroicons/react/24/solid";
+
+const ProjectCard = ({ projectNum }) => {
+  const [flip, setFlip] = useState(false);
+
+  return (
+    <div className={`card ${flip ? "flip" : ""} relative flex flex-col bg-gray h-[600px] w-[400px] border-2 border-navy rounded-[32px] overflow-clip shadow-xl`}>
+      
+{/* ------------------------- FRONTSIDE ------------------------- */}
+      
+      <div className="front bg-gray w-[400px] space-y-3 justify-content-center" onClick={() => setFlip(!flip)}>
+        {/* upper decoration */}
+        <div className="flex flex-row gap-2 pt-4 px-4">
+          <SparklesIcon className="size-12 text-navy"></SparklesIcon>
+          <div className="text-navy lg:text-md text-sm self-center">{data.name}</div>
+          <Divider className="text-navy self-center border-1 border-navy w-36"></Divider>
+          <div className="text-navy lg:text-md text-sm self-center">no. 0{projectNum}</div>
+        </div>
+        {/* title */}
+        <div className="px-4 h-16 content-center">
+          <div className="text-navy text-2xl text-fraunces self-start">{data[`project${projectNum}`]?.title}</div>
+        </div>
+        {/* image */}
+        <div className="flex flex-col relative w-[368px] mx-4 aspect-[3/2] border-2 border-navy rounded-[24px] self-center">
+          <Image className="rounded-[24px]" src={`/project-${projectNum}.png`} fill={true} sizes=""></Image>
+          {/* icon */}
+          <div className="absolute flex w-1/4 h-1/3 rounded-full border-2 border-b-navy border-r-navy bg-gray ml-[-4%] mt-[-4%] place-content-center">
+            <BookOpenIcon className="text-navy size-1/2 self-center py-1/4"></BookOpenIcon>
+          </div>
+          {/* status */}
+          <div className={`absolute flex w-1/3 h-1/6 rounded-full border-2 border-navy mr-[5%] mt-[-5%] ${data[`project${projectNum}`]?.active ? "bg-active" : "bg-inactive"} self-end place-content-center overflow-hidden`}>
+            <div className="sm:text-xs text-sm self-center justify-center text-gray">{data[`project${projectNum}`]?.active ? "ACTIVE" : "INACTIVE"}</div>
+          </div>
+        </div>
+        {/* self info */}
+        <div className="flex flex-row h-16 gap-2 py-2 px-4">
+          {/* role */}
+          <div className="flex w-56 h-full rounded-full border-2 border-navy bg-yellow self-end place-content-center overflow-hidden">
+            <div className="text-base self-center justify-center text-navy">{data[`project${projectNum}`]?.role}</div>
+          </div>
+        {/* dev stack */}
+          <div className="flex flex-row -space-x-4">
+            {data[`project${projectNum}`]?.stack.map((item, index) => {
+              return (
+                <div key={index} className="flex relative h-full aspect-square bg-gray border-2 border-navy z-10 hover:z-20 rounded-full">
+                  <Image src={`https://img.icons8.com/color/50/${item}.png`} fill={true}></Image>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {/* short desc */}
+        <div className="h-24 mx-4 p-2 border-2 border-navy rounded-[24px] text-navy overflow-scroll">
+          {data[`project${projectNum}`]?.desc}
+        </div>
+      </div>
+
+{/* ------------------------- BACKSIDE ------------------------- */}
+
+      <div className="back flex flex-col bg-gray" onClick={() => setFlip(!flip)}>
+        {/* upper decoration */}
+        <div className="flex flex-row gap-2 py-2">
+          <SparklesIcon className="size-12 text-navy"></SparklesIcon>
+          <div className="text-navy lg:text-md text-sm self-center">{data.name}</div>
+          <Divider className="text-navy self-center border-1 border-navy"></Divider>
+          <div className="text-navy lg:text-md text-sm self-center">no. {projectNum < 10 ? "0" : ""}{projectNum}</div>
+        </div>
+        {/* title */}
+        <div className="text-navy text-2xl text-fraunces px-4 pb-4 self-start">{data[`project${projectNum}`]?.title}</div>
+
+
+        {/* long desc */}
+        <div className="h-[470px] mx-4 mb-4 p-2 border-2 border-navy rounded-[24px] text-navy text-balance overflow-scroll overscroll-auto">
+          <p dangerouslySetInnerHTML={{ __html: data[`project${projectNum}`]?.longdesc }}></p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/app/work/page.js
+++ src/app/work/page.js
@@ -1,12 +1,11 @@
 "use client"
-import Image from "next/image";
 import { HomeIcon, IdentificationIcon, ArchiveBoxIcon, SparklesIcon, BookOpenIcon } from "@heroicons/react/24/solid";
 import Link from 'next/link'
 import {Divider} from "@nextui-org/divider";
-import { useState } from "react";
+import data from '/src/lib/en.json';
+import ProjectCard from "../components/ProjectCard";
 
 export default function Work() {
-  const [flip, setFlip] = useState(false);
 
   return (
     <main className="flex flex-row bg-beige min-h-screen">
@@ -24,86 +23,13 @@ export default function Work() {
         <Divider className="text-navy w-full place-self-center "></Divider>
 
         {/* Projects */}
-        <div className="card-grid grid md:grid-cols-2 grid-cols-1 gap-16 px-16">
-
-          {/* Project 6 */}
-          <div className={`card ${flip ? "flip" : ""} relative flex flex-col bg-gray aspect-[2/3] border-2 border-navy rounded-[32px] overflow-clip shadow-xl`}>
-            <div className="front bg-gray" onClick={() => setFlip(!flip)}>
-            {/* upper decoration */}
-              <div className="flex flex-row gap-2 py-2">
-                <SparklesIcon className="size-12 text-navy"></SparklesIcon>
-                <div className="text-navy lg:text-md text-sm self-center">Rina Reimer</div>
-                <Divider className="text-navy self-center border-1 border-navy"></Divider>
-                <div className="text-navy lg:text-md text-sm self-center">no. 06</div>
-              </div>
-            {/* title */}
-              <div className="text-navy text-2xl text-fraunces px-4 pb-4 self-start">PageByPage</div>
-            {/* image */}
-              <div className="flex flex-col relative w-full aspect-[3/2] border-2 border-navy rounded-lg self-center">
-                <Image className="rounded-lg" src="/project-6.png" fill={true}></Image>
-                {/* icon */}
-                <div className="absolute flex w-1/4 h-1/3 rounded-full border-2 border-b-navy border-r-navy bg-gray ml-[-4%] mt-[-4%] place-content-center">
-                  <BookOpenIcon className="text-navy size-1/2 self-center py-1/4"></BookOpenIcon>
-                </div>
-                {/* status */}
-                <div className="absolute flex w-1/3 h-1/6 rounded-lg border-2 border-navy mr-[5%] mt-[-5%] bg-active self-end place-content-center overflow-hidden">
-                  <div className="sm:text-xs text-sm self-center justify-center text-gray">ACTIVE</div>
-                </div>
-              </div>
-            {/* self info */}
-              <div className="flex flex-row h-[10%] gap-2 py-2 px-4">
-                {/* role */}
-                <div className="flex w-2/3 h-full rounded-lg border-2 border-navy bg-yellow self-end place-content-center overflow-hidden">
-                  <div className="sm:text-xs text-sm self-center justify-center text-navy">Fullstack Developer</div>
-                </div>
-                {/* dev stack */}
-                <div className="flex flex-row -space-x-2">
-                  <div className="flex relative h-full aspect-square bg-gray border-2 border-navy z-10 hover:z-20 rounded-full">
-                    <Image src={"https://img.icons8.com/color/50/46467A/figma--v1.png"} fill={true}></Image>
-                  </div>
-                  <div className="flex relative h-full aspect-square bg-gray border-2 border-navy z-10 hover:z-20 rounded-full">
-                    <Image src={"https://img.icons8.com/color/50/46467A/swift.png"} fill={true}></Image>
-                  </div>
-                  <div className="flex relative h-full aspect-square bg-gray border-2 border-navy z-10 hover:z-20 rounded-full">
-                    <Image src={"https://img.icons8.com/color/48/firebase.png"} fill={true}></Image>
-                  </div>
-                  <div className="flex relative h-full aspect-square bg-gray border-2 border-navy z-10 hover:z-20 rounded-full">
-                    <Image src={"https://img.icons8.com/color/48/google-books.png"} fill={true}></Image>
-                  </div> 
-                </div>
-              </div>
-
-              {/* short desc */}
-              <div className="h-2/5 mx-4 mb-4 p-2 border-2 border-navy rounded-lg text-navy">
-              blah blah blah a redesign of GoodReads but shhhh we dont want them to know that we secretly call it BetterReads
-              </div>
-            </div>
-            <div className="back flex flex-col bg-gray" onClick={() => setFlip(!flip)}>
-              {/* upper decoration */}
-              <div className="flex flex-row gap-2 py-2">
-                <SparklesIcon className="size-12 text-navy"></SparklesIcon>
-                <div className="text-navy lg:text-md text-sm self-center">Rina Reimer</div>
-                <Divider className="text-navy self-center border-1 border-navy"></Divider>
-                <div className="text-navy lg:text-md text-sm self-center">no. 06</div>
-              </div>
-            {/* title */}
-              <div className="text-navy text-2xl text-fraunces px-4 pb-4 self-start">PageByPage</div>
-            
-
-              {/* short desc */}
-              <div className="h-2/3 mx-4 mb-4 p-2 border-2 border-navy rounded-[24px] text-navy text-balance overscroll-auto">
-              blah blah blah a redesign of GoodReads but shhhh we dont want them to know that we secretly call it BetterReads
-              blah blah blah a redesign of GoodReads but shhhh we dont want them to know that we secretly call it BetterReads
-              blah blah blah a redesign of GoodReads but shhhh we dont want them to know that we secretly call it BetterReads
-              blah blah blah a redesign of GoodReads but shhhh we dont want them to know that we secretly call it BetterReads
-              blah blah blah a redesign of GoodReads but shhhh we dont want them to know that we secretly call it BetterReads
-              </div>
-
-            </div>
-          </div>
-          {/* Project 5 */}
-          <div className="bg-gray aspect-[2/3] border-2 border-navy rounded-[32px]">
-          </div>
+        <div className="card-grid grid md:grid-cols-2 grid-cols-1 gap-12 justify-items-center">
+          <ProjectCard name={[data.name]} projectNum={[6]} />
+          <ProjectCard name={[data.name]} projectNum={[5]} />
+          <ProjectCard name={[data.name]} projectNum={[4]} />
+          <ProjectCard name={[data.name]} projectNum={[3]} />
+          <ProjectCard name={[data.name]} projectNum={[2]} />
+          <ProjectCard name={[data.name]} projectNum={[1]} />
         </div>
 
       </div>
